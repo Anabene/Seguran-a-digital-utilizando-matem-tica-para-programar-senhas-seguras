cefbc00384ed593ee97de0f75b4a267acd595ec1
Anabene <165837536+Anabene@users.noreply.github.com>
2026-08-18 10:46:56 -0300

    Initialize password length and select buttons

--- main.js
+++ main.js
@@ -18,3 +18,10 @@ function aumentaTamanho(){
     tamanhoSenha = tamanhoSenha+1;
     numeroSenha.textContent = tamanhoSenha;
 }
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+console.log(botoes)
